f59a0486d16c343e6ae20c546782647583e5119f
Roman <roman@MacBook-Pro-Roman.local>
2020-07-24 19:08:09 +0300

    Finish Project

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,13 +1,37 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode,
-)
+const rootStyles = window.getComputedStyle( document.documentElement );
 
-FilePond.setOptions({
-    stylePanelAspectRation: 150 / 100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150
-})
+if ( rootStyles.getPropertyValue('--book-cover-width-large') != null  &&
+     rootStyles.getPropertyValue('--book-cover-width-large') != '' ) {
 
-FilePond.parse(document.body);
+    ready();
+
+} else {
+
+    document.getElementById('main-css').addEventListener('load', ready );
+
+}
+
+function ready() {
+
+    const coverWidth = parseFloat( rootStyles.getPropertyValue('--book-cover-width-large'));
+
+    const coverAspectRatio = parseFloat( rootStyles.getPropertyValue('--book-cover-aspect-ratio'));
+
+    const coverHeight = coverWidth / coverAspectRatio;
+
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode,
+    )
+    
+    FilePond.setOptions({
+        stylePanelAspectRation: 1 / coverAspectRatio,
+        imageResizeTargetWidth: coverWidth,
+        imageResizeTargetHeight: coverHeight
+    })
+    
+    FilePond.parse(document.body);
+
+}
+
